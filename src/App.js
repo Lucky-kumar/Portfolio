b5b572abe1dbbc8ee8bc03a04e94e6492271b442
Lucky Kumar <imt_2020051@iiitm.ac.in>
2022-07-09 10:15:15 +0530

    add projects heading

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Intro from './components/sections/Intro/Intro';
 import About from './components/sections/About/About';
 import Contact from './components/sections/Contact/Contact';
 import Projects from './components/sections/Projects/Projects';
+import ProjectsH from './components/sections/Projects/ProjectsH';
 
 function App() {
   return (
     <div className='container'>
       <Intro/>
       <About/>
+      <ProjectsH/>
       <Projects/>
       <Contact/>
     </div>
---
+++ src/components/sections/Projects/ProjectsH.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/ProjectsH.css'
+
+const ProjectsH = () => {
+    return (
+        <div className="ph_box">
+            <div className="ph_body">
+
+                <h1 className="ph_h1" data-heading="Projects">
+                    <span className="ph_span" contenteditable data-heading="Projects">Projects</span>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectsH
