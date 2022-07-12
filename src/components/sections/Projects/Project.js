b5415ce98f1bbfb7f21fe7e373b3fd7072b8dd42
Lucky Kumar <imt_2020051@iiitm.ac.in>
2022-07-12 13:16:52 +0530

    pbtn r

--- src/components/sections/Projects/Project.js
+++ src/components/sections/Projects/Project.js
@@ -19,9 +19,9 @@ const Project = ({ card_direction, name, description, url }) => {
                             {description}
                         </div>
                     </div>
-                    {/* <div className="pbtn_box">
+                    <div className="pbtn_box">
                     <PButton url={url} />
-                </div> */}
+                </div>
                     <div className="card_box">
                         <Card />
                     </div>
